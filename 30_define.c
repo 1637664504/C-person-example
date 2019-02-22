18d4c76a4f9c1e86dacefd33c43b64b260f3b394
liujiang <liujiang@tenda.cn>
2019-02-22 17:14:39 +0800

    1.git过滤 a.out 1 2文件
    2.debug宏的影响

--- 30_define.c
+++ 30_define.c
@@ -7,9 +7,17 @@
 #define BCM_DIAG_24G_WIRELESS_CONN      19
 #define BCM_DIAG_5G_WIRELESS_CONN      20
 
+//#define DEBUG
+#ifdef DEBUG
+#define debug printf
+#else
+#define debug
+#endif
+
 int main ( int argc, char *argv[] )
 {
-    printf("%d %d\n",BCM_DIAG_24G_WIRELESS_CONN,BCM_DIAG_5G_WIRELESS_CONN);
+	printf("11111\n");
+    debug("%d %d\n",BCM_DIAG_24G_WIRELESS_CONN,BCM_DIAG_5G_WIRELESS_CONN);
  
     
  return EXIT_SUCCESS;
