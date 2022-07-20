ab339d6c6fc707fe9fdb34858e981092530d44a0
Jiang-Liu <lj1637664504@outlook.com>
2022-07-20 07:55:22 -0400

    字符串解析实例更新

--- 16_string/15.4_sscanf.c
+++ 16_string/15.4_sscanf.c
@@ -5,11 +5,11 @@
 int main ( int argc, char *argv[] )
 {
     char buf[128] = "2408:8557:3031:4031:16af:f5a7:a192:1cd4/64";
-    char ip[16] = {0};
-    char mac[18] = {0};
+    char ip[64] = {0};
+    int mask=0;
 
-    sscanf(buf,"%s",ip);
-    printf("ip=%s\n",ip);
+    sscanf(buf,"%[^/]/%d",ip,&mask);
+    printf("ip=%s,mask=%d\n",ip,mask);
 
     return 0;
 }
