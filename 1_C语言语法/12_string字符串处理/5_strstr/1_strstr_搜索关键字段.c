01cd8d80e98a712c22c19caf2d87a8ab3c19b112
liuJiang <lj1637664504@outlook.com>
2024-01-26 11:31:50 +0800

    strstr搜索实例

--- 1_C语言语法/12_string字符串处理/5_strstr/1_strstr_搜索关键字段.c
+++ 1_C语言语法/12_string字符串处理/5_strstr/1_strstr_搜索关键字段.c
@@ -10,23 +10,24 @@ static int handler_wifi(const char* cmd)
     char* p_start = buf;
     char* p_end = buf;
 
-    // cmd=#VSLAM SET WIFI,ruichi-test,:ij2200862..;
+    // cmd=WIFI,ruichi-liuj,ij2200862;
     strncpy(buf, cmd, sizeof(buf) - 1);
     p_start = strstr(buf, "WIFI,")+5;
-    p_end = strstr(p_start, ",:");
+    p_end = strchr(p_start, ',');
     strncpy(wifi, p_start, p_end - p_start);
 
     // pass
-    p_start = p_end+2;
+    p_start = p_end+1;
     p_end = strchr(p_start, ';');
     strncpy(pass, p_start, p_end - p_start);
+    printf("%s,%s\n", wifi, pass);
 
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    char buf[128] = "#VSLAM SET WIFI,ruichi-test,:ij2200862..;";
+    char buf[128] = "WIFI,ruichi-liuj,ij2200862;";
     handler_wifi(buf);
 
     return 0;
